895b65efeb2212f6c517a69313d1db0ca88db5b9
hifadullah <mr.hifad10@gmail.com>
2024-11-08 23:13:04 +0530

    error fixed

--- src/Router/Routers.js
+++ src/Router/Routers.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { createBrowserRouter, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Home from '../Pages/Home';
 import Tours from '../Pages/Tours';
 import TourDetails from '../Pages/TourDetails';
